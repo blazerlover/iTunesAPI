d9266f544a5eb20b475259d47cafc1277832d34a
blazerlover <pashkovich1@yandex.ru>
2020-05-25 12:03:42 +0300

    First step

---
+++ app/src/main/java/ru/exemple/itunesapi/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.exemple.itunesapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
